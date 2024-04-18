6270980f1929890dc135f4f2f75ca7e9f5e98d39
Raymundo Peña <raymundopea00@gmail.com>
2024-04-18 15:28:45 -0400

    add parsered date in task details

--- task-managment-service/routes/taskRoutes.js
+++ task-managment-service/routes/taskRoutes.js
@@ -30,7 +30,7 @@ router.get('/:id', async(req, res) => {
             return res.status(404).json({ message: 'Task not found' });
         }
 
-        res.json(task)
+        res.json({...task, date: dateHelpers.parseDateToString(task.date)})
     }catch(err){
         res.status(500).json({
             message: "Interval server error"
