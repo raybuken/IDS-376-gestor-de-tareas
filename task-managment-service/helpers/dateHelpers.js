2a088b5737396e36dbd0b30084de77725bd11875
Raymundo Peña <raymundopea00@gmail.com>
2024-04-18 15:18:31 -0400

    fix date format

--- task-managment-service/helpers/dateHelpers.js
+++ task-managment-service/helpers/dateHelpers.js
@@ -8,25 +8,15 @@ function parseStringToDate(string) {
 }
 
 function parseDateToString(dateString) {
-    var parts = dateString.split(' ');
+    const dateObject = new Date(dateString);
 
-    var dateParts = parts[0].split('-');
-    var year = parseInt(dateParts[0]);
-    var month = parseInt(dateParts[1]) - 1;
-    var day = parseInt(dateParts[2]);
+    const day = dateObject.getUTCDate();
+    const month = dateObject.getUTCMonth() + 1;
+    const year = dateObject.getUTCFullYear();
 
-    var timeParts = parts[1] ? parts[1].split(':') : [0, 0, 0];
-    var hours = parseInt(timeParts[0]);
-    var minutes = parseInt(timeParts[1]);
-    var seconds = parseInt(timeParts[2]);
+    const formattedDate = `${day < 10 ? '0' : ''}${day}/${month < 10 ? '0' : ''}${month}/${year}`;
 
-    var date = new Date(year, month, day, hours, minutes, seconds);
-
-    var formattedDay = date.getDate().toString().padStart(2, '0');
-    var formattedMonth = (date.getMonth() + 1).toString().padStart(2, '0');
-    var formattedYear = date.getFullYear();
-
-    return formattedDay + '/' + formattedMonth + '/' + formattedYear;
+    return formattedDate
 }
 
 module.exports = {
